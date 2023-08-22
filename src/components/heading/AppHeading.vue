16fed7398bab87f09d2b12c5251ec4cf7a52efcd
i.shunyaev <i.shunyaev@fact.digital>
2023-08-22 17:36:01 +0400

    [feat] added heading

---
+++ src/components/heading/AppHeading.vue
@@ -0,0 +1,38 @@
+<template>
+ <h2
+  :class="['heading', {'uppercase': uppercase}]"
+  >
+  <slot></slot>
+ </h2>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  uppercase: Boolean
+})
+</script>
+
+<style lang="scss" scoped>
+  .heading {
+    position: relative;
+    @include text(17px, 28px, 400);
+    color: $white;
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: -5px;
+      left: 0;
+      right: 0;
+      height: 2px;
+      background-color: $green-line;
+      box-shadow: (0px 0px 10px #01B09B);
+    }
+
+    &.uppercase {
+      text-transform: uppercase;
+    }
+  }
+</style>
