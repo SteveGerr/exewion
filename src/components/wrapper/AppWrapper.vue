30c3ee72cc1fc2664738ce946a6ec277e6838ab9
i.shunyaev <i.shunyaev@fact.digital>
2023-08-22 17:58:47 +0400

    [feat] added wrapper

---
+++ src/components/wrapper/AppWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+  .app-wrapper {
+    display: flex;
+    padding: 8px 12px;
+    border-radius: 1.5rem;
+    background-color: $background-color;
+  }
+</style>
