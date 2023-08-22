e00cb12f8cea86c1fcd8758b28ccceb4f16be974
i.shunyaev <i.shunyaev@fact.digital>
2023-08-22 17:56:33 +0400

    [feat] added progresbar

---
+++ src/components/progressbar/ProgressBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="['progress-bar', {'simple': simple}]">
+    <div
+      class="progress-bar__fill"
+      :style="[{'width': `${props.value}%`}]"
+      >
+    </div>
+    <div v-if="!simple" class="progress-bar__percent">50%/100%</div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  value: Number,
+  simple: Boolean
+})
+</script>
+<style lang="scss" scoped>
+.progress-bar {
+  display: flex;
+  width: 100%;
+  height: 72px;
+  position: relative;
+  border-radius: 100px;
+  overflow: hidden;
+  background-color: $lilac-back;
+
+  &.simple {
+    height: 4px;
+  }
+
+  &__fill {
+    background: $green;
+    box-shadow: 0px 0px 16px 0px $green;
+  }
+
+  &__percent {
+    width: 330px;
+    position: absolute;
+    top: calc(50% - 15px);
+    left: calc(50% - 115px);
+    @include text(30px, normal, 600);
+    letter-spacing: 0.325px;
+    color: $white;
+    text-align: center;
+  }
+}
+</style>
