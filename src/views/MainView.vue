07b36378453702d68baaf706c42e6b63ee350ecc
i.shunyaev <i.shunyaev@fact.digital>
2023-08-23 08:01:06 +0400

    [feat] added main template

---
+++ src/views/MainView.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="main">
+    <AppHeader></AppHeader>
+    <div class="main__body">
+        <AppSidebar :items="sidebarItems" :current="route.path">
+        </AppSidebar>
+        <AppWrapper class="main__content">
+          <RouterView></RouterView>
+        </AppWrapper>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import AppWrapper from '@/components/wrapper/AppWrapper.vue'
+import AppSidebar from '@/components/sidebar/AppSidebar.vue'
+import AppHeader from '@/components/header/AppHeader.vue'
+import RocketIcon from '@/components/icons/RocketIcon.vue'
+import ManIcon from '@/components/icons/ManIcon.vue'
+import BagIcon from '@/components/icons/BagIcon.vue'
+import WalletIcon from '@/components/icons/WalletIcon.vue'
+import ChipIcon from '@/components/icons/ChipIcon.vue'
+
+const route = useRoute()
+
+const sidebarItems = [
+  {
+    id: 1,
+    icon: RocketIcon,
+    route: '/start',
+    active: false
+  },
+  {
+    id: 2,
+    icon: ChipIcon,
+    route: '/stock',
+    active: true
+  },
+  {
+    id: 3,
+    icon: BagIcon,
+    route: '/portfolio',
+    active: false
+  },
+  {
+    id: 4,
+    icon: WalletIcon,
+    route: '/wallet',
+    active: false
+  },
+  {
+    id: 5,
+    icon: ManIcon,
+    route: '/account',
+    active: false
+  }
+]
+</script>
+<style lang="scss" scoped>
+  .main {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    height: 100%;
+    padding: 0 0 30px 10px;
+
+    &__body {
+      display: flex;
+      width: 100%;
+      height: 100%;
+      gap: 40px;
+    }
+
+    &__content {
+      width: 100%;
+      max-width: 710px;
+    }
+  }
+</style>
