8a45427f404fece0597f04a4ae358972df20307f
i.shunyaev <i.shunyaev@fact.digital>
2023-08-23 08:04:05 +0400

    [refactor] removed trash

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
   },
   {
     path: '/ui-kit',
