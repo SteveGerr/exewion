50e729f5a0d975189118302869addbf75fd84abd
i.shunyaev <i.shunyaev@fact.digital>
2023-08-23 07:58:28 +0400

    [feat] added sidebar item

---
+++ src/components/sidebar/SidebarItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="['sidebar-item', {'active': active}]">
+    <div class="sidebar-item__icon">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  active: Boolean
+})
+</script>
+<style lang="scss" scoped>
+.sidebar-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 30px;
+  margin: 8px 0;
+  position: relative;
+  cursor: pointer;
+
+  &.active {
+    &:after {
+      content: '';
+      position: absolute;
+      left: -20px;
+      top: 0;
+      bottom: 0;
+      right: -25px;
+      background: linear-gradient(270deg, #6024DE 0%, #7B37FF 100%);
+      border-radius: 0px 20px 20px 0px;
+      z-index: 1;
+    }
+  }
+
+  &__icon {
+    width: 100%;
+    max-width: 25px;
+    height: 25px;
+    object-fit: contain;
+    z-index: 2;
+  }
+}
+</style>
