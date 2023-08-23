7522952646311d562da43e18700fad584b158be4
i.shunyaev <i.shunyaev@fact.digital>
2023-08-23 08:04:30 +0400

    [feat] added steps

---
+++ src/components/steps/StepFour.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="start">
+      <div class="start__wrapper">
+        Step 4
+      </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+  .start {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 710px;
+    height: 100%;
+    color: $gray;
+
+    &__rocket {
+      width: 51px;
+      height: 73px;
+      margin: 0 0 35px 0;
+    }
+
+    &__button {
+      max-width: 239px;
+      font-size: 30px;
+      margin: 0 0 10px 0;
+    }
+
+    &__wrapper {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      margin: auto 0;
+    }
+
+    &__link {
+      flex: 15px;
+      color: $lilac-light;
+    }
+
+    &__steps {
+      margin: 0 0 23px 0;
+    }
+  }
+
+</style>
