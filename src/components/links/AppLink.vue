510a8abe4d5b9710de4475a84e599951b62d25a4
i.shunyaev <i.shunyaev@fact.digital>
2023-08-23 07:58:07 +0400

    [feat] added link

---
+++ src/components/links/AppLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <a
+    class="app-link"
+    :href="href"
+    target="_blank"
+    rel="noopener noreferrer"
+    >
+    <slot></slot>
+    </a>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  href: String
+})
+
+</script>
+<style lang="scss" scoped>
+  .app-link {
+    @include text(16px, 28px, 400);
+    color: $lilac;
+    text-transform: none;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
