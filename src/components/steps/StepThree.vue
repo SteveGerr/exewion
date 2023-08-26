54fde6adb709a2d050256000c054536deb5e55ef
i.shunyaev <i.shunyaev@fact.digital>
2023-08-26 09:32:49 +0400

    [feat] added step three

--- src/components/steps/StepThree.vue
+++ src/components/steps/StepThree.vue
@@ -1,23 +1,43 @@
 <template>
-  <div class="start">
-      <div class="start__wrapper">
-        Step 3
+  <div class="step-three">
+      <div class="step-three__wrapper">
+        <img class="step-three__rocket" :src="Rocket" alt="">
+        <Progressbar class="step-three__progressbar" :value="ProgressValue"></Progressbar>
+        <p class="step-three__text">осталось совсем немного</p>
       </div>
   </div>
 </template>
+
 <script setup>
+import { ref } from 'vue'
+import Rocket from '@/assets/svg/rocket.svg'
+import Progressbar from '@/components/progressbar/ProgressBar.vue'
+import { useStepsStore } from '@/store/stepsStore'
+
+const store = useStepsStore()
+
+const { changeStep } = store
+
+const ProgressValue = ref(50)
+
+setInterval(() => {
+  ProgressValue.value += 10
+}, 1000)
+
+setTimeout(() => {
+  changeStep()
+}, 5000)
 
 </script>
 
 <style lang="scss">
-  .start {
+  .step-three {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
     width: 100%;
     max-width: 710px;
-    height: 100%;
     color: $gray;
 
     &__rocket {
@@ -26,28 +46,23 @@
       margin: 0 0 35px 0;
     }
 
-    &__button {
-      max-width: 239px;
-      font-size: 30px;
-      margin: 0 0 10px 0;
-    }
-
     &__wrapper {
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
       width: 100%;
-      margin: auto 0;
+      margin: 19.5rem 0 25rem 0;
     }
 
-    &__link {
-      flex: 15px;
-      color: $lilac-light;
+    &__progressbar {
+      max-width: 595px;
     }
 
-    &__steps {
-      margin: 0 0 23px 0;
+    &__text {
+      margin: 5px 0 0 0;
+      @include paragraph;
+      text-decoration: underline;
     }
   }
 
