b353390f7af893372695777527b770b51c66e8bc
i.shunyaev <i.shunyaev@fact.digital>
2023-08-26 09:30:53 +0400

    [feat] added range

--- src/components/range/AppRange.vue
+++ src/components/range/AppRange.vue
@@ -1,20 +1,24 @@
 <template>
-  <input
-    class="range"
-    :value="modelValue"
-    @input="updateValue"
-    type="range"
-    :min="0"
-    :max="100"
-  />
+  <div class="range">
+    <input
+      class="range__input"
+      :value="modelValue"
+      @input="updateValue"
+      type="range"
+      :min="min"
+      :max="max"
+      :step="step"
+    />
+  </div>
 </template>
 <script setup>
 import { defineProps, defineEmits } from 'vue'
 
 defineProps({
-  modelValue: String,
+  modelValue: Number,
   min: Number,
-  max: Number
+  max: Number,
+  step: Number
 })
 
 const emit = defineEmits(['update:modelValue'])
@@ -27,51 +31,59 @@ const updateValue = (e) => {
 </script>
 <style lang="scss" scoped>
 .range {
-  $trackHeight: 6px;
-  $thumbRadius: 6px;
+  width: 100%;
 
-  position: relative;
-  appearance: none;
-  border-radius: 999px;
-  z-index: 0;
+  &__input {
+    $trackHeight: 6px;
+    $thumbRadius: 6px;
 
-  &::before {
-    content: "";
-    position: absolute;
     width: 100%;
-    height: 100%;
-    background: $lilac-back;
-    pointer-events: none;
-    border-radius: 999px;
-  }
-
-  &::-webkit-slider-runnable-track {
-    appearance: none;
-    background: $lilac-back;
-    height: $trackHeight;
-    border-radius: 999px;
-  }
-
-  &::-moz-range-track {
-    appearance: none;
-    background: $lilac-back;
-    height: $trackHeight;
-    border-radius: 999px;
-  }
-
-  &::-webkit-slider-thumb {
     position: relative;
-    top: 50%;
-    transform: translate(0, -50%);
-    width: $thumbRadius;
-    height: 25px;
-    background: $green;
-    border-radius: 999px;
-    box-shadow: 0px 0px 16px rgba(72, 174, 154, 0.70);
-    pointer-events: all;
     appearance: none;
-    z-index: 1;
-    cursor: pointer;
+    border-radius: 10px;
+    z-index: 0;
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: $lilac-back;
+      pointer-events: none;
+      border-radius: 10px;
+    }
+
+    &::-webkit-slider-runnable-track {
+      appearance: none;
+      background: $lilac-back;
+      height: $trackHeight;
+      border-radius: 10px;
+      outline: none;
+    }
+
+    &::-moz-range-track {
+      appearance: none;
+      background: $lilac-back;
+      height: $trackHeight;
+      max-height: $trackHeight;
+      border-radius: 10px;
+      outline: none;
+    }
+
+    &::-webkit-slider-thumb {
+      position: relative;
+      top: 50%;
+      transform: translate(0, -50%);
+      width: $thumbRadius;
+      height: 25px;
+      background: $green;
+      border-radius: 10px;
+      box-shadow: 0px 0px 16px rgba(72, 174, 154, 0.70);
+      pointer-events: all;
+      appearance: none;
+      z-index: 1;
+      cursor: pointer;
+    }
   }
 }
 
