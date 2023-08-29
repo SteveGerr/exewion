982ebcd812b457135147f68137079763ff4c3906
i.shunyaev <i.shunyaev@fact.digital>
2023-08-29 22:48:08 +0400

    [feat] added aliases

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+  configureWebpack: {
+    resolve: {
+
+      alias: {
+        process: 'process/browser',
+        stream: 'stream-browserify',
+        zlib: 'browserify-zlib',
+        util: 'util'
+      }
+    }
+  },
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/exewion/'
