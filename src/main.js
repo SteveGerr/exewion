fae755863290844430e85b210be5fee4b2f09bc7
i.shunyaev <i.shunyaev@fact.digital>
2023-08-29 22:47:50 +0400

    [feat] added socket service, Buffer, EventEmitter, init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import { Buffer } from 'buffer/'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import socketService from './websockets/socket.service'
+import process from 'process'
+import EventEmitter from 'events'
+
+window.Buffer = Buffer
+window.process = process
+window.EventEmitter = EventEmitter
+
+socketService.init({}, 'wss://madinium.app:8443/')
 
 const pinia = createPinia()
 
