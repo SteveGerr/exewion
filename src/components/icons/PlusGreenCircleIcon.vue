6db50d18ae98db3d52ca5d1e1e478703adb9699e
i.shunyaev <i.shunyaev@fact.digital>
2023-08-29 13:03:32 +0400

    [feat] added svg component

---
+++ src/components/icons/PlusGreenCircleIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_1401_6)">
+    <circle cx="12" cy="12" r="7" fill="#00B09B"/>
+    </g>
+    <path d="M15.92 11.48V12.92H8.752V11.48H15.92ZM13.072 8.744L13.056 15.656H11.616V8.744H13.072Z" fill="white"/>
+    <defs>
+    <filter id="filter0_d_1401_6" x="0" y="0" width="24" height="24" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+    <feOffset/>
+    <feGaussianBlur stdDeviation="2.5"/>
+    <feComposite in2="hardAlpha" operator="out"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.690196 0 0 0 0 0.607843 0 0 0 1 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1401_6"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1401_6" result="shape"/>
+    </filter>
+    </defs>
+  </svg>
+</template>
