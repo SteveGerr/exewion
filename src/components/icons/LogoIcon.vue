494978d45a15647df598cbdcdccfa9c92185f3eb
i.shunyaev <i.shunyaev@fact.digital>
2023-08-29 13:03:45 +0400

    [feat] added svg component

---
+++ src/components/icons/LogoIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg width="133" height="133" viewBox="0 0 133 133" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_1401_2)">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M65.9592 16.5789H66.1644C93.7561 16.5789 116.124 38.9464 116.124 66.5379V66.7554C116.124 94.3472 93.7561 116.715 66.1644 116.715H65.9592C38.3675 116.715 16 94.3472 16 66.7554V66.5379C16 38.9464 38.3675 16.5789 65.9592 16.5789Z" fill="url(#paint0_linear_1401_2)"/>
+    </g>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M42.8734 55.0554L52.9239 45.2657L29.0987 21.6913L9 21.5352L42.8734 55.0554ZM118 108.515L97.9015 108.359L53.7684 64.6922L97.3724 22.2223L117.471 22.3785L73.8697 64.8484L118 108.515ZM52.9237 85.3763L29.8695 107.83L9.7693 107.674L42.8732 75.4342L52.9237 85.3763Z" fill="#F4F4F4"/>
+    <defs>
+    <filter id="filter0_d_1401_2" x="0" y="0.578613" width="132.124" height="132.136" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+    <feOffset/>
+    <feGaussianBlur stdDeviation="8"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0.411765 0 0 0 0 0.164706 0 0 0 0 0.913725 0 0 0 1 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1401_2"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1401_2" result="shape"/>
+    </filter>
+    <linearGradient id="paint0_linear_1401_2" x1="32.6531" y1="35.7755" x2="98.2245" y2="104.469" gradientUnits="userSpaceOnUse">
+    <stop stop-color="#6A2BEA"/>
+    <stop offset="1" stop-color="#6326E1"/>
+    </linearGradient>
+    </defs>
+  </svg>
+</template>
