a96d52e2b32650314f195c0b9a3a05ac02c8cc18
i.shunyaev <i.shunyaev@fact.digital>
2023-09-03 22:01:44 +0400

    [feat] added notifications

---
+++ src/components/notifications/AppNotifications.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul v-if="show" class="notifications">
+    <slot></slot>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  show: Boolean
+})
+
+</script>
+<style lang="scss" scoped>
+  .notifications {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 10px;
+  }
+
+</style>
