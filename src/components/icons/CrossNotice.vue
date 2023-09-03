3e2756d0f5a6e5102c54afb0fc52ea66920d6115
i.shunyaev <i.shunyaev@fact.digital>
2023-09-03 22:02:18 +0400

    [feat] added cross notice

---
+++ src/components/icons/CrossNotice.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="7" height="7" viewBox="0 0 7 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <line x1="1.06115" y1="0.75" x2="5.99989" y2="5.68874" stroke="#896AD7" stroke-width="1.5" stroke-linecap="round"/>
+    <line x1="6.00049" y1="1.06005" x2="1.06115" y2="5.99939" stroke="#896AD7" stroke-width="1.5" stroke-linecap="round"/>
+  </svg>
+</template>
